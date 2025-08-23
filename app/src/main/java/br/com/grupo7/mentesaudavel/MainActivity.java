b42770fc389a1ae00106aec38a8fcd112d0bce1e
Igor Gassen <igorhgassen@gmail.com>
2025-08-23 13:57:53 -0300

    Criação do projeto Mobile com Java para o sistema Mente Saudável

---
+++ app/src/main/java/br/com/grupo7/mentesaudavel/MainActivity.java
@@ -0,0 +1,11 @@
+package br.com.grupo7.mentesaudavel;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    protected void OnCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
