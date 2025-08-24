a97b78d5f8613cf597ed1127db097fb27a104da2
Igor Gassen <igorhgassen@gmail.com>
2025-08-24 14:32:23 -0300

    Rascunho da tela de login. Correção no método onCreate da MainActivity

--- app/src/main/java/br/com/grupo7/mentesaudavel/MainActivity.java
+++ app/src/main/java/br/com/grupo7/mentesaudavel/MainActivity.java
@@ -4,7 +4,8 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    protected void OnCreate(Bundle savedInstanceState) {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
