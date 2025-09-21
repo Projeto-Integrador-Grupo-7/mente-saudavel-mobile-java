cc369747ed66c3f69e0a0d01d23f42b5ae8ef2e0
Igor Gassen <igorhgassen@gmail.com>
2025-09-21 11:42:12 -0300

    Adição de gráfico de pizza na tela de Dashboard + Chamada API

--- app/src/main/java/br/com/grupo7/mentesaudavel/api/ApiInterface.java
+++ app/src/main/java/br/com/grupo7/mentesaudavel/api/ApiInterface.java
@@ -1,6 +1,7 @@
 package br.com.grupo7.mentesaudavel.api;
 
 import java.util.List;
+import java.util.Map;
 
 import br.com.grupo7.mentesaudavel.model.DashboardRequest;
 import br.com.grupo7.mentesaudavel.model.Questionario;
@@ -11,4 +12,7 @@ import retrofit2.http.POST;
 public interface ApiInterface {
     @POST("dashboard/historico")
     Call<List<Questionario>> getQuestionariosRespondidos(@Body DashboardRequest request);
+
+    @POST("dashboard/graficoPizza")
+    Call<Map<String, Integer>> getQtdeUsuariosPorEstratificacao(@Body DashboardRequest request);
 }
