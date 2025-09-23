250b706c3a60699fb46cc1dd2daa551bfea64e55
Daniel <danielkloh06@gmail.com>
2025-09-23 20:30:57 -0300

    resposta do questionario

--- app/src/main/java/br/com/grupo7/mentesaudavel/activity/QuestionarioActivity.java
+++ app/src/main/java/br/com/grupo7/mentesaudavel/activity/QuestionarioActivity.java
@@ -1,5 +1,6 @@
 package br.com.grupo7.mentesaudavel.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,7 +16,9 @@ public class QuestionarioActivity extends AppCompatActivity {
 
     private RadioGroup[] perguntas = new RadioGroup[20];
     private Button btnEnviar;
-    private TextView txtResultado;
+    // private TextView txtResultado; // Removido, pois o resultado será exibido em ResultadoActivity
+
+    public static final String EXTRA_RESULTADO_SIM = "VALOR_RESULTADO_SIM"; // Chave para o Intent extra
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,18 +33,19 @@ public class QuestionarioActivity extends AppCompatActivity {
         }
 
         btnEnviar = findViewById(R.id.btnEnviar);
-        txtResultado = findViewById(R.id.txtResultado);
+        // txtResultado = findViewById(R.id.txtResultado); // Removido
 
         btnEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                calcularResultado();
+                calcularResultadoEChamarResultadoActivity();
             }
         });
     }
 
-    private void calcularResultado() {
-        int sim = 0, nao = 0;
+    private void calcularResultadoEChamarResultadoActivity() {
+        int sim = 0;
+        // int nao = 0; // Variável 'nao' não é mais necessária para passar para a próxima activity
 
         for (RadioGroup rg : perguntas) {
             int selectedId = rg.getCheckedRadioButtonId();
@@ -49,13 +53,20 @@ public class QuestionarioActivity extends AppCompatActivity {
                 RadioButton resposta = findViewById(selectedId);
                 if (resposta.getText().toString().equalsIgnoreCase("Sim")) {
                     sim++;
-                } else {
-                    nao++;
                 }
+                // else { // Não precisamos mais contar 'nao' separadamente para este fluxo
+                // nao++;
+                // }
             }
         }
 
-        txtResultado.setText("Respostas SIM: " + sim + "\nRespostas NÃO: " + nao);
+        // Remover a exibição do resultado nesta activity
+        // txtResultado.setText("Respostas SIM: " + sim + "\nRespostas NÃO: " + nao);
+
+        // Chamar a ResultadoActivity, passando a contagem de "Sim"
+        Intent intent = new Intent(QuestionarioActivity.this, ResultadoActivity.class);
+        intent.putExtra(EXTRA_RESULTADO_SIM, sim); // Passa a contagem de "Sim"
+        startActivity(intent);
     }
 
 }
