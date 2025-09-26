43d924fa0c08785df7f4656f8daf3ef0a6911365
schvengberGabriel <gabrielcostaschvengber@gmail.com>
2025-09-26 06:44:36 -0300

    Realizada página de RELATORIO aonde faz conexão com Páginas de DashBoard e Retorna a Inicial(home).

---
+++ app/src/main/java/br/com/grupo7/mentesaudavel/activity/RelatorioActivity.java
@@ -0,0 +1,120 @@
+package br.com.grupo7.mentesaudavel.activity;
+
+import static java.lang.Integer.*;
+
+import br.com.grupo7.mentesaudavel.R;
+import br.com.grupo7.mentesaudavel.api.ApiClient;
+import br.com.grupo7.mentesaudavel.api.ApiInterface;
+import br.com.grupo7.mentesaudavel.model.DashboardRequest;
+import br.com.grupo7.mentesaudavel.model.Questionario;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import java.util.List;
+
+public class RelatorioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relatorio);
+
+        buscarRelatorio();
+    }
+
+    private void buscarRelatorio() {
+        ApiInterface apiInterface = ApiClient.getApiInterface();
+
+        DashboardRequest request = new DashboardRequest("daba7458-bbac-4643-8b13-359e68440b5e");
+
+        Call<List<Questionario>> call = apiInterface.getQuestionariosRespondidos(request);
+
+        Callback<List<Questionario>> callback = callbackRelatorio();
+
+        call.enqueue(callback);
+    }
+
+    private Callback<List<Questionario>> callbackRelatorio() {
+        return new Callback<>() {
+            @Override
+            public void onResponse(@NonNull Call<List<Questionario>> call, @NonNull Response<List<Questionario>> response) {
+                if (response.isSuccessful()) {
+                    popularRelatorio(response.body());
+                } else {
+                    ApiClient.tratarErroHttp(RelatorioActivity.this, response.code());
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<List<Questionario>> call, @NonNull Throwable error) {
+                ApiClient.tratarFalhaConexao(RelatorioActivity.this, error);
+            }
+        };
+    }
+
+    private void popularRelatorio(List<Questionario> listaQuestionariosRespondidos) {
+        runOnUiThread(() -> {
+
+            TextView textRelatorio = this.findViewById(R.id.textViewBodyRelatorio);
+
+            String msgErro = validaDadosQuestionario(listaQuestionariosRespondidos);
+            if (msgErro != null) {
+                textRelatorio.setText(msgErro);
+                return;
+            }
+
+            int pontos = getPontuacaoUltimoQuestionario(listaQuestionariosRespondidos);
+
+            String mensagem;
+            if (pontos <= 5) {
+                mensagem = getString(R.string.relatorioBaixo);
+            } else if (pontos <= 10) {
+                mensagem = getString(R.string.relatorioLeve);
+            } else if (pontos <= 15) {
+                mensagem = getString(R.string.relatorioModerado);
+            } else {
+                mensagem = getString(R.string.relatorioGrave);
+            }
+
+            textRelatorio.setText(mensagem);
+        });
+    }
+
+    private String validaDadosQuestionario(List<Questionario> listaQuestionariosRespondidos) {
+        String textRelatorio = null;
+
+        if (listaQuestionariosRespondidos == null || listaQuestionariosRespondidos.isEmpty()) {
+            textRelatorio = this.getString(R.string.msgQuestionarioNaoEncontrado);
+        }
+
+        return textRelatorio;
+    }
+
+    private int getPontuacaoUltimoQuestionario(List<Questionario> listaQuestionariosRespondidos) {
+        Questionario ultimo = listaQuestionariosRespondidos.get(listaQuestionariosRespondidos.size() - 1);
+        String pontosStr = ultimo.pontuacao;
+
+        return parseInt(pontosStr);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    public void redirectToPaginaInicial(View view) {
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+    }
+}
